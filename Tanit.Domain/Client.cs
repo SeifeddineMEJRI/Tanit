a9d84b67ea2080bb3657f287f0e22fb48b82ea14
Seifeddine MEJRI <mej.seifeddine@gmail.com>
2024-01-10 19:18:49 +0100

    Fix Web app Project and Add references

---
+++ Tanit.Domain/Client.cs
@@ -0,0 +1,10 @@
+namespace Tanit.Domain;
+
+public class Client
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
+}
