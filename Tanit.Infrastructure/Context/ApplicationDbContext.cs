f7bb567d6eca1a6c0f159b270d1cd03c422c2cfc
Seifeddine MEJRI <mej.seifeddine@gmail.com>
2024-01-12 20:23:49 +0100

    Create migration script

--- Tanit.Infrastructure/Context/ApplicationDbContext.cs
+++ Tanit.Infrastructure/Context/ApplicationDbContext.cs
@@ -14,5 +14,19 @@ public class ApplicationDbContext : IdentityDbContext<TanitUser, TanitRole, stri
 
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        foreach (var property in builder.Model.GetEntityTypes()
+                     .SelectMany(t => t.GetProperties())
+                     .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+        {
+            property.SetColumnType("decimal(18,2)");
+        }
+
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
     //public DbSet<Client> Clients => Set<Client>();
 }
